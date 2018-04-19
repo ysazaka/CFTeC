8798bdd61571c1e05977f1f188ee146c2ce0a95a
alerario <alerario@gmail.com>
2018-04-19 18:07:32 -0300

    Update create_tables.sql

--- script/database/create_tables.sql
+++ script/database/create_tables.sql
@@ -1,2 +1,9 @@
 -- script para criacao de tabelas no banco de dados
 --
+CREATE TABLE public.teste
+(
+    codigo integer NOT NULL,
+    nome character varying(255),
+    CONSTRAINT teste_pkey PRIMARY KEY (codigo)
+)
+-- As tabelas corretas devem ser criadas a partir daqui.
