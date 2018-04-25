b204b4585fbd5a8211b6f04f5c040f42f67e2d40
sirnande <sirnande.lima@hotmail.com>
2018-04-25 20:29:55 -0300

    Update create_tables.sql

--- script/database/create_tables.sql
+++ script/database/create_tables.sql
@@ -15,3 +15,9 @@ CREATE TABLE public.message(
     codigo integer NOT NULL,
      CONSTRAINT mensage_pkey PRIMARY KEY (codigo)
 )
+
+
+-- Responsavel Sirnande dos Santos Lima
+CREATE  TABLE  public .project (
+)
+
