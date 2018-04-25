d59479f09e03fd0d0aacc30e924bb09d9deec779
sirnande <sirnande.lima@hotmail.com>
2018-04-25 20:38:09 -0300

    Update create_tables.sql

--- script/database/create_tables.sql
+++ script/database/create_tables.sql
@@ -8,28 +8,29 @@ CREATE TABLE public.teste
     codigo integer NOT NULL,
     nome character varying(255),
     CONSTRAINT teste_pkey PRIMARY KEY (codigo)
-)
+);
 -- As tabelas corretas devem ser criadas a partir daqui.
---Responsavel: Diego Vilella
-CREATE TABLE public.message(
-    codigo integer NOT NULL,
-     CONSTRAINT mensage_pkey PRIMARY KEY (codigo)
-)
-
 
 -- Responsavel Sirnande dos Santos Lima
 CREATE  TABLE  public.project (
     codigo integer NOT NULL,
     nome varchar(45), 
     descrição varchar(120)
-)
+);
+
+--Responsavel: Diego Vilella
+CREATE TABLE public.message(
+    codigo integer NOT NULL,
+     CONSTRAINT mensage_pkey PRIMARY KEY (codigo)
+);
+
 
 
 -- Responsavel Danilo Missao Morita
 create table public.host (
     codigo integer NOT NULL,
     Constraint codigo_pkey PRIMARY KEY (codigo)
-)
+);
 
 
 -- Athos Castro Moreno
@@ -40,4 +41,4 @@ CREATE TABLE public.content
     tipo_mensagem text,
     conteudo text,
     constraint content_pkey primary key (codigo)
-)
+);
